585680f67db1bc0deffd03b0b559dc29e1e9b73e
Joostlek <joostlek@outlook.com>
2018-05-16 14:17:48 +0200

    Initial commit

---
+++ src/main/java/nl/hu/v1wac/firstapp/webservices/WorldResource.java
@@ -0,0 +1,31 @@
+package nl.hu.v1wac.firstapp.webservices;
+
+import com.google.gson.Gson;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+@Path("/countries")
+public class WorldResource  {
+    @GET
+    @Produces("application/json")
+    public String getOrders() {
+        Gson gson = new Gson();
+        return gson.toJson(ServiceProvider.getWorldService().getAllCountries());
+    }
+
+    @GET
+    @Path("{code}")
+    @Produces("application/json")
+    public String getApplicationByCode(@PathParam("code") String code) {
+        Gson gson = new Gson();
+        if (code.equals("largestsurfaces")) {
+            return gson.toJson(ServiceProvider.getWorldService().get10LargestSurfaces());
+        } else if (code.equals("largestpopulations")) {
+            return gson.toJson(ServiceProvider.getWorldService().get10LargestPopulations());
+        }
+        return gson.toJson(ServiceProvider.getWorldService().getCountryByCode(code));
+    }
+}
