c53331bfd7197e9211ab887b9387dd1a13203b8e
Joostlek <joostlek@outlook.com>
2018-06-06 16:11:19 +0200

    Finished

--- src/main/java/nl/hu/v1wac/firstapp/persistence/PostgresBaseDao.java
+++ src/main/java/nl/hu/v1wac/firstapp/persistence/PostgresBaseDao.java
@@ -1,19 +1,46 @@
 package nl.hu.v1wac.firstapp.persistence;
 
+import java.net.URI;
+import java.sql.Connection;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
-import java.sql.Connection;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 
 public class PostgresBaseDao {
-    Connection getConnection() {
-        Connection result = null;
-        try {
-            InitialContext ic = new InitialContext();
-            DataSource ds = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
-            result = ds.getConnection();
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
+    private static DataSource connectionPool;
+
+    public PostgresBaseDao() {
+        if (connectionPool == null) {
+            try {
+                final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
+
+                if (DATABASE_URL_PROP != null) { // de applicatie draait op Heroku
+                    URI dbUri = new URI(DATABASE_URL_PROP);
+                    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+                    BasicDataSource pool = new BasicDataSource();
+
+                    if (dbUri.getUserInfo() != null) {
+                        pool.setUsername(dbUri.getUserInfo().split(":")[0]);
+                        pool.setPassword(dbUri.getUserInfo().split(":")[1]);
+                    }
+
+                    pool.setMaxIdle(4); // maximaal 4 ongebruikte connecties
+                    pool.setMaxTotal(20); // maximaal 20 connecties in Heroku
+                    pool.setDriverClassName("org.postgresql.Driver");
+                    pool.setUrl(dbUrl);
+                    pool.setInitialSize(1);
+
+                    connectionPool = pool;
+                } else { // de applicatie draait lokaal
+                    InitialContext ic = new InitialContext();
+                    connectionPool = (DataSource)ic.lookup("java:comp/env/jdbc/PostgresDS");
+                }
+            } catch (Exception e) { throw new RuntimeException(e); }
         }
-        return result;
+    }
+    protected final Connection getConnection() {
+        try {
+            return connectionPool.getConnection();
+        } catch (Exception ex) { throw new RuntimeException(ex); }
     }
 }
